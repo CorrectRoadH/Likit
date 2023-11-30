54b18563526b70c7cda4121e59147ea92707cf40
CorrectRoadH <a778917369@gmail.com>
2023-11-30 16:44:22 +0800

    feat: add http server

--- go.mod
+++ go.mod
@@ -9,15 +9,22 @@ require (
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	github.com/labstack/echo/v4 v4.11.3 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/onsi/ginkgo/v2 v2.13.2 // indirect
 	github.com/onsi/gomega v1.30.0 // indirect
 	github.com/redis/go-redis/v9 v9.3.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/dig v1.17.0 // indirect
 	go.uber.org/fx v1.20.1 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- internal/adapter/in/restful/vote_server.go
+++ internal/adapter/in/restful/vote_server.go
@@ -1,17 +1,78 @@
 package restful
 
-import "github.com/CorrectRoadH/Likit/internal/port/in"
+import (
+	"fmt"
+	"os"
+
+	"github.com/CorrectRoadH/Likit/internal/port/in"
+	"github.com/labstack/echo/v4"
+)
 
 type VoteServer struct {
 	voteUseCase in.VoteUseCase
+	e           *echo.Echo
 }
 
 func NewVoteServer(voteUseCase in.VoteUseCase) *VoteServer {
-	return &VoteServer{
+	e := echo.New()
+
+	s := &VoteServer{
 		voteUseCase: voteUseCase,
+		e:           e,
 	}
+
+	s.register(e.Group("/api/v1"))
+
+	return s
+}
+
+func (v *VoteServer) Vote(c echo.Context) error {
+	// TODO get value from body
+
+	businessId := c.Param("businessId")
+	messageId := c.Param("messageId")
+	userId := c.QueryParam("userId")
+
+	return v.voteUseCase.Vote(c.Request().Context(), businessId, messageId, userId)
+}
+
+func (v *VoteServer) UnVote(c echo.Context) error {
+	return nil
+}
+
+func (v *VoteServer) Count(c echo.Context) error {
+	businessId := c.Param("businessId")
+	messageId := c.Param("messageId")
+
+	count, err := v.voteUseCase.Count(c.Request().Context(), businessId, messageId)
+	if err != nil {
+		// return 400 or 500 error
+		return err
+	}
+
+	// need to define a struct for response
+	return c.JSON(200, count)
+}
+
+func (v *VoteServer) ListUser(c echo.Context) error {
+	return nil
+}
+
+func (v *VoteServer) register(g *echo.Group) error {
+	g.GET("/vote/:businessId/:messageId", v.Count)
+	g.GET("/list/:businessId/:messageId", v.ListUser)
+
+	g.POST("/vote", v.Vote)
+	g.POST("/unvote", v.UnVote)
+
+	return nil
 }
 
 func (v *VoteServer) Start() error {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	v.e.Start(fmt.Sprintf(":%s", port))
 	return nil
 }
